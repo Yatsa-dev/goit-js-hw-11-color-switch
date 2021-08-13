26f32dce7d458615724e5c601ea2d315cdee929f
Ihor Yatsyshyn <74863323+Yatsa-dev@users.noreply.github.com>
2021-08-13 14:22:32 +0300

    done task

---
+++ index.js
@@ -0,0 +1,42 @@
+'use stricts'
+const colors = [
+    '#FFFFFF',
+    '#2196F3',
+    '#4CAF50',
+    '#FF9800',
+    '#009688',
+    '#795548',
+];
+const DELAY = 1000;
+const refs = {
+    startBtn: document.querySelector('button[data-action="start"]'),
+    stopBtn: document.querySelector('button[data-action="stop"]'),
+    bodyBackgroundColor: document.querySelector('body')
+}
+
+refs.startBtn.addEventListener('click', onChangeColorBody)
+
+const randomIntegerFromInterval = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+function onChangeColorBody() {
+    const intervalID = setInterval(() => {
+        getRandomColorBody()
+    }, DELAY)
+    if (intervalID) {
+        refs.startBtn.setAttribute('disabled', true);
+    }
+    refs.stopBtn.addEventListener('click', () => {
+        clearInterval(intervalID);
+        refs.startBtn.removeAttribute('disabled')
+    })
+}
+
+
+function getRandomColorBody() {
+    const color = randomIntegerFromInterval(0, colors.length - 1);
+    refs.bodyBackgroundColor.style.backgroundColor = colors[color];
+};
+
+
